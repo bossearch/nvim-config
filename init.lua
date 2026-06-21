699fc822b6f1cf71f29f667dd4fd3edb71c6b13b
bossearch <yudha.bosse@gmail.com>
2026-06-21 23:34:52 +0800

    chore(clean): fix indenting and remove unused line

--- init.lua
+++ init.lua
@@ -1,20 +1,17 @@
 vim.loader.enable()
 
-vim.cmd.packadd("nvim.undotree")
-vim.cmd.packadd("nvim.difftool")
-
 local extra_dirs = { "core", "lib" }
 local config_dir = vim.fn.stdpath("config")
 for _, dir_name in ipairs(extra_dirs) do
-	local full_path = config_dir .. "/lua/" .. dir_name
-	if vim.fn.isdirectory(full_path) == 1 then
-		for name, type in vim.fs.dir(full_path) do
-			if type == "file" and name:match("%.lua$") then
-				local module = dir_name .. "." .. name:gsub("%.lua$", "")
-				require(module)
-			end
-		end
-	end
+    local full_path = config_dir .. "/lua/" .. dir_name
+    if vim.fn.isdirectory(full_path) == 1 then
+        for name, type in vim.fs.dir(full_path) do
+            if type == "file" and name:match("%.lua$") then
+                local module = dir_name .. "." .. name:gsub("%.lua$", "")
+                require(module)
+            end
+        end
+    end
 end
 
 require("lib.util").get_nvim_startup_time()
--- lua/lib/util.lua
+++ lua/lib/util.lua
@@ -2,76 +2,76 @@ local util = {}
 
 -- usercmd --
 util.copy_to_clipboard = function(content)
-	vim.fn.setreg("+", content)
-	vim.notify('Copied "' .. content .. '" to the clipboard!', vim.log.levels.INFO)
+    vim.fn.setreg("+", content)
+    vim.notify('Copied "' .. content .. '" to the clipboard!', vim.log.levels.INFO)
 end
 
 -- usercmd --
 util.get_root_dir = function()
-	local bufname = vim.fn.expand("%:p")
-	if vim.fn.filereadable(bufname) == 0 then
-		return
-	end
+    local bufname = vim.fn.expand("%:p")
+    if vim.fn.filereadable(bufname) == 0 then
+        return
+    end
 
-	local parent = vim.fn.fnamemodify(bufname, ":h")
-	local git_root = vim.fn.systemlist("git -C " .. parent .. " rev-parse --show-toplevel")
-	if #git_root > 0 and git_root[1] ~= "" then
-		return git_root[1]
-	else
-		return parent
-	end
+    local parent = vim.fn.fnamemodify(bufname, ":h")
+    local git_root = vim.fn.systemlist("git -C " .. parent .. " rev-parse --show-toplevel")
+    if #git_root > 0 and git_root[1] ~= "" then
+        return git_root[1]
+    else
+        return parent
+    end
 end
 
 -- startuptime --
 util.get_nvim_startup_time = function()
-	local function send_event()
-		vim.api.nvim_exec_autocmds("User", {
-			pattern = "startuptime-loaded",
-		})
-	end
+    local function send_event()
+        vim.api.nvim_exec_autocmds("User", {
+            pattern = "startuptime-loaded",
+        })
+    end
 
-	local function get_ms_from_file()
-		local file = io.open("/tmp/nvim-startup.log", "r")
-		local prev_line = nil
-		local last_line = nil
-		for line in file:lines() do
-			prev_line = last_line
-			last_line = line
-		end
-		if not prev_line then
-			return
-		end
-		local raw_ms = prev_line:match("([%d.]+)%s+[%d.]+:%s+%-+ NVIM STARTED %-+")
-		return tonumber(raw_ms)
-	end
+    local function get_ms_from_file()
+        local file = io.open("/tmp/nvim-startup.log", "r")
+        local prev_line = nil
+        local last_line = nil
+        for line in file:lines() do
+            prev_line = last_line
+            last_line = line
+        end
+        if not prev_line then
+            return
+        end
+        local raw_ms = prev_line:match("([%d.]+)%s+[%d.]+:%s+%-+ NVIM STARTED %-+")
+        return tonumber(raw_ms)
+    end
 
-	local function clear_file()
-		local file = io.open("/tmp/nvim-startup.log", "w")
-		file:close()
-	end
+    local function clear_file()
+        local file = io.open("/tmp/nvim-startup.log", "w")
+        file:close()
+    end
 
-	local timer = vim.uv.new_timer()
-	timer:start(
-		10,
-		50,
-		vim.schedule_wrap(function()
-			if done then
-				return
-			end
-			local ms = get_ms_from_file()
-			if ms and ms > 10 then
-				done = true
-				util._startup_time = string.format("%.2f ms", ms)
-				send_event()
-				clear_file()
-			elseif not ms then
-				done = true
-				util._startup_time = "???"
-				send_event()
-				clear_file()
-			end
-		end)
-	)
+    local timer = vim.uv.new_timer()
+    timer:start(
+        10,
+        50,
+        vim.schedule_wrap(function()
+            if done then
+                return
+            end
+            local ms = get_ms_from_file()
+            if ms and ms > 10 then
+                done = true
+                util._startup_time = string.format("%.2f ms", ms)
+                send_event()
+                clear_file()
+            elseif not ms then
+                done = true
+                util._startup_time = "???"
+                send_event()
+                clear_file()
+            end
+        end)
+    )
 end
 
 -- tips --
