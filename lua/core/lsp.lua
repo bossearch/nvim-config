6c2ca965d8ea8a4c6cf863581d05beb7a4e55f49
bossearch <yudha.bosse@gmail.com>
2026-06-21 23:36:47 +0800

    feat(lsp): enable bashls & add doc highlight with auto format

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,8 +1,8 @@
-vim.lsp.enable({ "lua_ls", "nixd" })
+vim.lsp.enable({ "lua_ls", "nixd", "bashls" })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("blink-cmp").get_lsp_capabilities(capabilities)
 
 vim.lsp.config("*", {
-	capabilities = capabilities,
+    capabilities = capabilities,
 })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,50 @@
 return {
-    spec = {
-        src = "https://github.com/neovim/nvim-lspconfig",
-    },
+    spec = { src = "https://github.com/neovim/nvim-lspconfig" },
     "nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
+    after = function()
+        local group = vim.api.nvim_create_augroup("lsp", {})
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+                vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+                vim.keymap.set({ 'n', 'v' }, 'gra', vim.lsp.buf.code_action)
+                vim.keymap.set('n', 'grD', vim.lsp.buf.declaration)
+
+                local client = vim.lsp.get_client_by_id(args.data.client_id)
+                if client == nil then
+                    return
+                end
+                -- document highlight
+                if client:supports_method(
+                        vim.lsp.protocol.Methods.textDocument_documentHighlight
+                    ) then
+                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                        group = group,
+                        buffer = args.buf,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                        group = group,
+                        buffer = args.buf,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                end
+                -- lsp format on save
+                if client:supports_method(
+                        vim.lsp.protocol.Methods.textDocument_formatting
+                    ) then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = group,
+                        buffer = args.buf,
+                        callback = function()
+                            vim.lsp.buf.format({
+                                async = true,
+                                bufnr = args.buf,
+                            })
+                        end,
+                    })
+                end
+            end,
+        })
+    end
 }
