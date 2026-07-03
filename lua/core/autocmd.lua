30cb6e4bd2ec6116f06715f60cd4c31b6c8fcd5f
bossearch <yudha.bosse@gmail.com>
2026-07-03 09:05:44 +0800

    refactor(autocmd): add local autocmd & augroups function

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,15 +1,18 @@
 local autocmd = vim.api.nvim_create_autocmd
+local function augroup(name)
+    return vim.api.nvim_create_augroup("user" .. name, { clear = true })
+end
 
--- Highlight on yank
 autocmd("TextYankPost", {
+    group = augroup("highlight_on_yank"),
     pattern = { "*" },
     callback = function()
         vim.hl.on_yank()
     end,
 })
 
--- Remove trailing whitespace on save
 autocmd("BufWritePre", {
+    group = augroup("remove_trailing_whitespace_on_save"),
     pattern = { "*" },
     callback = function()
         local save_cursor = vim.fn.getpos(".")
@@ -20,15 +23,15 @@ autocmd("BufWritePre", {
     end,
 })
 
--- Resize splits on window resize
 autocmd("VimResized", {
+    group = augroup("resize_splits_on_window_resize"),
     callback = function()
         vim.cmd("tabdo wincmd =")
     end,
 })
 
--- Auto-create directory before saving file
 autocmd("BufWritePre", {
+    group = augroup("auto_create_directory_before_saving_file"),
     callback = function(event)
         local file = vim.uv.fs_realpath(event.match) or event.match
         if vim.bo.filetype == "oil" then
@@ -38,34 +41,34 @@ autocmd("BufWritePre", {
     end,
 })
 
--- Auto nohlsearch
-autocmd('CursorMoved', {
-    group = vim.api.nvim_create_augroup('auto-nohlsearch', { clear = true }),
+autocmd("CursorMoved", {
+    group = augroup("auto_nohlsearch"),
     callback = function()
         if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
-            vim.schedule(function() vim.cmd("nohlsearch") end)
+            vim.schedule(function()
+                vim.cmd("nohlsearch")
+            end)
         end
-    end
-})
-
--- Remove annoying auto add comment
-autocmd({ "FileType", "BufEnter" }, {
-    callback = function()
-        vim.opt_local.formatoptions:remove("cro")
     end,
 })
 
--- But im not annoyed for this ft
 autocmd("FileType", {
-    pattern = { "markdown", "gitcommit" },
-    callback = function()
-        vim.opt_local.formatoptions:append("cro")
+    group = augroup("formatoptions"),
+    pattern = "*",
+    callback = function(args)
+        vim.opt_local.formatoptions:remove("r")
+        vim.opt_local.formatoptions:remove("o")
+        if vim.tbl_contains({ "markdown", "gitcommit" }, args.match) then
+            vim.opt_local.formatoptions:append("r")
+            vim.opt_local.formatoptions:append("o")
+            vim.opt_local.spell = true
+        end
     end,
 })
-
-vim.api.nvim_create_autocmd("User", {
+autocmd("User", {
+    group = augroup("startuptime-loaded"),
     pattern = "startuptime-loaded",
     callback = function()
         vim.cmd("Alpha")
-    end
+    end,
 })
