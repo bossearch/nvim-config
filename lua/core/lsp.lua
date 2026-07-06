3bf248a5bb2d7a2c393e56ff511d74eeaa265ca7
bossearch <yudha.bosse@gmail.com>
2026-07-06 12:06:54 +0800

    fix(lsp): remove global capabilities override

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,8 +1,1 @@
 vim.lsp.enable({ "lua_ls", "nixd", "bashls" })
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("blink-cmp").get_lsp_capabilities(capabilities)
-
-vim.lsp.config("*", {
-    capabilities = capabilities,
-})
