d91d441f98d3624a722b0d1aadd5fcebb98c3f62
bossearch <yudha.bosse@gmail.com>
2026-07-09 17:24:26 +0800

    refactor(blink.cmp): simplify configuration
    - convert blink-cmp dependencies into separate lazy-loaded specs
    - move LSP capability setup into blink-cmp initialization
    - simplify completion sources and scoring configuration
    - remove unused git and words completion sources
    - update blink-ripgrep loading behavior

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,205 +1,167 @@
 return {
-    "blink.cmp",
-    spec = {
-        src = "https://github.com/saghen/blink.cmp",
-        version = "v1",
-        dependencies = {
-            "https://github.com/Kaiser-Yang/blink-cmp-git",
-            "https://github.com/archie-judd/blink-cmp-words",
-            "https://github.com/mikavilpas/blink-ripgrep.nvim",
+    {
+        "blink.cmp",
+        spec = {
+            src = "https://github.com/saghen/blink.cmp",
+            version = "v1",
         },
-    },
-    event = "InsertEnter",
-    after = function()
-        require("blink-cmp").setup({
-            appearance = {
-                kind_icons = {
-                    Class = "",
-                    Color = "",
-                    Constant = "",
-                    Constructor = "",
-                    Copilot = "",
-                    Enum = "",
-                    EnumMember = "",
-                    Event = "",
-                    Field = "",
-                    File = "",
-                    Folder = "",
-                    Function = "󰊕",
-                    Interface = "",
-                    Keyword = "",
-                    Method = "",
-                    Module = "󱒌",
-                    Operator = "",
-                    Property = "",
-                    Reference = "",
-                    Snippet = "",
-                    Struct = "",
-                    Text = "",
-                    TypeParameter = "",
-                    Unit = "",
-                    Value = "",
-                    Variable = "",
-                    Warning = "",
+        event = "InsertEnter",
+        before = function()
+            require("lz.n").trigger_load("blink-ripgrep.nvim")
+        end,
+        after = function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
+
+            local score = {
+                buffer = 50,
+                lsp = 80,
+                path = 70,
+                ripgrep = 50,
+                snippets = 90,
+            }
+
+            require("blink.cmp").setup({
+                appearance = {
+                    kind_icons = {
+                        Class = "",
+                        Color = "",
+                        Constant = "",
+                        Constructor = "",
+                        Copilot = "",
+                        Enum = "",
+                        EnumMember = "",
+                        Event = "",
+                        Field = "",
+                        File = "",
+                        Folder = "",
+                        Function = "󰊕",
+                        Interface = "",
+                        Keyword = "",
+                        Method = "",
+                        Module = "󱒌",
+                        Operator = "",
+                        Property = "",
+                        Reference = "",
+                        Snippet = "",
+                        Struct = "",
+                        Text = "",
+                        TypeParameter = "",
+                        Unit = "",
+                        Value = "",
+                        Variable = "",
+                        Warning = "",
+                    },
+                    nerd_font_variant = "normal",
                 },
-                nerd_font_variant = "normal",
-            },
-            cmdline = {
+                cmdline = { keymap = { preset = "inherit" } },
                 completion = {
-                    list = { selection = { auto_insert = true, preselect = true } },
-                    menu = { auto_show = true },
-                },
-                enabled = true,
-                keymap = { preset = "inherit" },
-                sources = function()
-                    local type = vim.fn.getcmdtype()
-                    if type == "/" or type == "?" then
-                        return { "buffer" }
-                    end
-                    if type == ":" or type == "@" then
-                        return { "cmdline" }
-                    end
-                    return {}
-                end,
-            },
-            completion = {
-                accept = { auto_brackets = { enabled = false } },
-                documentation = { auto_show = true, auto_show_delay_ms = 200 },
-                ghost_text = { enabled = false },
-                list = { selection = { auto_insert = true, preselect = true } },
-                menu = {
-                    auto_show = true,
-                    draw = {
-                        columns = { { "label", "label_description", gap = 1 }, { "kind", gap = 1, "kind_icon" } },
-                        padding = { 1, 0 },
-                    },
-                },
-            },
-            keymap = {
-                preset = "none",
-                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-e>"] = { "hide", "fallback" },
-                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-                ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-                ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-                ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-                ["<C-y>"] = { "select_and_accept", "fallback" },
-                ["<S-Tab>"] = { "snippet_backward", "fallback" },
-                ["<Tab>"] = { "snippet_forward", "fallback" },
-            },
-            signature = { enabled = false },
-            -- snippets = { preset = "luasnip" },
-            sources = {
-                default = { "buffer", "dictionary", "lsp", "git", "path", "ripgrep", "snippets", "thesaurus" },
-                providers = {
-                    buffer = {
-                        async = true,
-                        enabled = true,
-                        max_items = 4,
-                        min_keyword_length = 2,
-                        module = "blink.cmp.sources.buffer",
-                        name = "buff",
-                        opts = {
-                            get_bufnrs = function()
-                                return vim.tbl_filter(function(bufnr)
-                                    return vim.bo[bufnr].buftype == ""
-                                end, vim.api.nvim_list_bufs())
-                            end,
-                        },
-                        score_offset = 60,
-                    },
-                    dictionary = {
-                        enabled = function()
-                            return vim.tbl_contains({ "gitcommit", "markdown" }, vim.bo.filetype)
-                        end,
-                        module = "blink-cmp-words.dictionary",
-                        name = "dict",
-                        opts = {
-                            definition_pointers = { "!", "&", "^" },
-                            dictionary_search_threshold = 3,
-                            score_offset = 40,
-                        },
-                    },
-                    git = {
-                        enabled = function()
-                            return vim.tbl_contains({ "gitcommit" }, vim.bo.filetype)
-                        end,
-                        min_keyword_length = 1,
-                        module = "blink-cmp-git",
-                        name = "git",
-                        score_offset = 90,
-                    },
-                    lsp = {
-                        async = true,
-                        enabled = true,
-                        fallbacks = { "buffer" },
-                        min_keyword_length = 0,
-                        module = "blink.cmp.sources.lsp",
-                        name = "lsp",
-                        score_offset = 90,
-                        timeout_ms = 2000,
-                    },
-                    path = {
-                        async = true,
-                        enabled = true,
-                        fallbacks = { "buffer" },
-                        min_keyword_length = 0,
-                        module = "blink.cmp.sources.path",
-                        name = "path",
-                        opts = {
-                            get_cwd = function(_)
-                                return vim.fn.getcwd()
-                            end,
-                            label_trailing_slash = true,
-                            show_hidden_files_by_default = true,
-                            trailing_slash = false,
-                        },
-                        score_offset = 70,
-                    },
-                    ripgrep = {
-                        async = true,
-                        enabled = true,
-                        module = "blink-ripgrep",
-                        name = "grep",
-                        opts = {
-                            context_size = 5,
-                            debug = false,
-                            fallback_to_regex_highlighting = true,
-                            max_filesize = "1M",
-                            prefix_min_len = 3,
-                            project_root_fallback = true,
-                            project_root_marker = ".git",
-                            search_casing = "--ignore-case",
-                        },
-                        score_offset = 50,
-                    },
-                    -- snippets = {
-                    --     async = false,
-                    --     enabled = true,
-                    --     max_items = 5,
-                    --     min_keyword_length = 2,
-                    --     module = "blink.cmp.sources.snippets",
-                    --     name = "snip",
-                    --     opts = { show_autosnippets = true, use_show_condition = true },
-                    --     score_offset = 80,
-                    -- },
-                    thesaurus = {
-                        enabled = function()
-                            return vim.tbl_contains({ "gitcommit", "markdown" }, vim.bo.filetype)
-                        end,
-                        module = "blink-cmp-words.thesaurus",
-                        name = "thes",
-                        opts = {
-                            definition_pointers = { "!", "&", "^" },
-                            score_offset = 20,
-                            similarity_depth = 2,
-                            similarity_pointers = { "&", "^" },
+                    accept = { auto_brackets = { enabled = false } },
+                    documentation = { auto_show = true },
+                    menu = {
+                        draw = {
+                            columns = { { "label", "label_description", gap = 1 }, { "kind", gap = 1, "kind_icon" } },
+                            padding = { 1, 0 },
                         },
                     },
                 },
-            },
-            term = { enabled = false },
-        })
-    end,
+                keymap = {
+                    preset = "none",
+                    ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                    ["<C-e>"] = { "hide", "fallback" },
+                    ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+                    ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+                    ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+                    ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+                    ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+                    ["<C-y>"] = { "select_and_accept", "fallback" },
+                    ["<S-Tab>"] = { "snippet_backward", "fallback" },
+                    ["<Tab>"] = { "snippet_forward", "fallback" },
+                },
+                -- snippets = { preset = "luasnip" },
+                sources = {
+                    default = { "lsp", "snippets", "path", "ripgrep" },
+                    providers = {
+                        buffer = {
+                            enabled = true,
+                            async = true,
+                            min_keyword_length = 2,
+                            module = "blink.cmp.sources.buffer",
+                            opts = {
+                                get_bufnrs = function()
+                                    return vim.tbl_filter(function(bufnr)
+                                        return vim.bo[bufnr].buftype == ""
+                                    end, vim.api.nvim_list_bufs())
+                                end,
+                            },
+                            score_offset = score.buffer,
+                        },
+                        lsp = {
+                            enabled = true,
+                            async = true,
+                            fallbacks = { "buffer" },
+                            min_keyword_length = 0,
+                            module = "blink.cmp.sources.lsp",
+                            name = "lsp",
+                            score_offset = score.lsp,
+                            timeout_ms = 2000,
+                        },
+                        path = {
+                            enabled = true,
+                            async = true,
+                            fallbacks = { "buffer" },
+                            min_keyword_length = 0,
+                            module = "blink.cmp.sources.path",
+                            name = "path",
+                            opts = {
+                                get_cwd = function(_)
+                                    return vim.fn.getcwd()
+                                end,
+                                label_trailing_slash = true,
+                                show_hidden_files_by_default = true,
+                                trailing_slash = false,
+                            },
+                            score_offset = score.path,
+                        },
+                        ripgrep = {
+                            enabled = true,
+                            async = true,
+                            fallbacks = { "buffer" },
+                            module = "blink-ripgrep",
+                            name = "grep",
+                            opts = {
+                                prefix_min_len = 2,
+                                backend = {
+                                    use = "gitgrep",
+                                    max_filesize = "1M",
+                                    project_root_fallback = true,
+                                },
+                                debug = false,
+                            },
+                            score_offset = score.ripgrep,
+                        },
+                        snippets = {
+                            enabled = true,
+                            async = true,
+                            min_keyword_length = 0,
+                            module = "blink.cmp.sources.snippets",
+                            name = "snip",
+                            opts = { show_autosnippets = true, use_show_condition = true },
+                            score_offset = score.snippets,
+                        },
+                    },
+                },
+                term = { enabled = false },
+            })
+        end,
+    },
+    {
+        "blink-ripgrep.nvim",
+        spec = { src = "https://github.com/mikavilpas/blink-ripgrep.nvim" },
+        lazy = true,
+    },
 }
