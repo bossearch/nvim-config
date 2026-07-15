f5390c313d4cf0c0b1d00eb4f141b9f2f7fc7bbd
bossearch <yudha.bosse@gmail.com>
2026-07-15 18:06:34 +0800

    feat(lsp): add lazy load for lsp

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,13 +1,19 @@
-local util = require("lib.util")
-local servers_to_enable = {}
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("lazy_load_lsp", { clear = true }),
+    callback = function(args)
+        local util = require("lib.util")
+        local servers_to_enable = {}
 
-util.scan_modules("lua/lang", "lang.", function(lang)
-    if lang.lsp then
-        for server_name, config in pairs(lang.lsp) do
-            vim.lsp.config(server_name, config)
-            table.insert(servers_to_enable, server_name)
-        end
-    end
-end)
+        util.scan_modules("lua/lang", "lang.", function(lang)
+            if lang.lsp then
+                for server_name, config in pairs(lang.lsp) do
+                    vim.lsp.config(server_name, config)
+                    table.insert(servers_to_enable, server_name)
+                end
+            end
+        end)
 
-vim.lsp.enable(servers_to_enable)
+        vim.lsp.enable(servers_to_enable)
+        vim.api.nvim_del_augroup_by_id(args.group)
+    end,
+})
