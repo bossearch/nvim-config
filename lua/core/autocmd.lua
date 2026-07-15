8e745af1b9372872d32a3373b41b0a04a3a5ba6c
bossearch <yudha.bosse@gmail.com>
2026-07-15 18:01:59 +0800

    fix(autocmd): remove restore cursor position autocmd
    by removing this allow nice transition when using snacks picker custom
    full preview window to the real buffer

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -55,22 +55,6 @@ autocmd("FileType", {
     end,
 })
 
-autocmd("BufReadPost", {
-    group = augroup("return_to_last_cursor_position"),
-    callback = function()
-        if vim.o.diff then
-            return
-        end
-        local last_pos = vim.api.nvim_buf_get_mark(0, '"')
-        local last_line = vim.api.nvim_buf_line_count(0)
-        local row = last_pos[1]
-        if row < 1 or row > last_line then
-            return
-        end
-        pcall(vim.api.nvim_win_set_cursor, 0, last_pos)
-    end,
-})
-
 autocmd("TermOpen", {
     group = augroup("custom_terminal"),
     callback = function()
