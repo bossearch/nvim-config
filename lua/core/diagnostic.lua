ecec5498568cd9150f95dbc1bcfd448383f9d34b
bossearch <yudha.bosse@gmail.com>
2026-07-15 18:22:11 +0800

    feat(diagnostic): auto-open floating diagnostics

--- lua/core/diagnostic.lua
+++ lua/core/diagnostic.lua
@@ -1,40 +1,47 @@
 vim.diagnostic.config({
-	enable = true,
-	signs = true,
-	float = {
-		source = false,
-		header = "",
-		scope = "line",
-		prefix = "",
-		suffix = "",
-		format = function(diagnostic)
-			return diagnostic.message
-		end,
-	},
-	severity_sort = true,
+    enable = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticVirtualTextError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticVirtualTextWarn",
+            [vim.diagnostic.severity.HINT] = "DiagnosticVirtualTextHint",
+            [vim.diagnostic.severity.INFO] = "DiagnosticVirtualTextInfo",
+        },
+    },
+    float = {
+        source = false,
+        border = "solid",
+        header = "",
+        scope = "line",
+        prefix = "",
+        suffix = "",
+        format = function(diagnostic)
+            return diagnostic.message
+        end,
+    },
+    underline = false,
+    severity_sort = true,
 })
 
+-- Auto open diagnostic floating window
 vim.api.nvim_create_autocmd("CursorHold", {
-	callback = function()
-		vim.diagnostic.open_float(nil, { focusable = false })
-	end,
+    group = vim.api.nvim_create_augroup("DiagnosticHover", { clear = true }),
+    callback = function()
+        vim.diagnostic.open_float(nil, {
+            focusable = false,
+            close_events = {
+                "BufLeave",
+                "CursorMoved",
+                "DiagnosticChanged",
+                "InsertEnter",
+                "WinLeave",
+            },
+        })
+    end,
 })
-
-local ns = vim.api.nvim_create_namespace("diag_highest_only")
-local orig = vim.diagnostic.handlers.signs
-vim.diagnostic.handlers.signs = {
-	show = function(_, bufnr, _, opts)
-		local diagnostics = vim.diagnostic.get(bufnr)
-		local worst_per_line = {}
-		for _, d in ipairs(diagnostics) do
-			local existing = worst_per_line[d.lnum]
-			if not existing or d.severity < existing.severity then
-				worst_per_line[d.lnum] = d
-			end
-		end
-		orig.show(ns, bufnr, vim.tbl_values(worst_per_line), opts)
-	end,
-	hide = function(_, bufnr)
-		orig.hide(ns, bufnr)
-	end,
-}
