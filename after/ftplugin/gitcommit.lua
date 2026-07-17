51e45c70563ba0790d879269cf1f25e073dbe411
bossearch <yudha.bosse@gmail.com>
2026-07-17 13:35:02 +0800

    fix(ftplugin): add vim.schedule for gitcommit

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,8 +1,10 @@
-vim.opt_local.colorcolumn = { "50", "72" }
-vim.opt_local.comments = {
-    "b:*",
-    "b:-",
-}
-vim.opt_local.spell = true
-vim.opt_local.textwidth = 72
-vim.opt_local.wrap = true
+vim.schedule(function()
+    vim.opt_local.colorcolumn = { "50", "72" }
+    vim.opt_local.comments = {
+        "b:*",
+        "b:-",
+    }
+    vim.opt_local.spell = true
+    vim.opt_local.textwidth = 72
+    vim.opt_local.wrap = true
+end)
